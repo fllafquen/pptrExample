c0037e14feca9edc7b4c440777181417a2415e9b
PF3DLKVD\Consultor <fllafquen.externo@sii.cl>
2022-07-12 12:04:38 -0400

    se elimina código basura.

--- testCoop/example.test.js
+++ testCoop/example.test.js
@@ -45,17 +45,3 @@ describe('My first Puppeteer Test', () => {
     })
 })
 
-
-/*const page = await browser.newPage()
-        await page.goto("https://devexpress.github.io/testcafe/example/")
-        await page.type("#developer-name", 'Francisco', {delay: 70})
-        await page.waitFor(5000)
-        await page.click('#tried-test-cafe', {clickCount: 1})
-        await page.select('#preferred-interface', 'JavaScript API')
-        const message = 'Lets fill that message with some text'
-        await page.type('#comments', message)
-        await page.click('#submit-button')
-        await page.waitForSelector('.result-content')
-        await page.waitFor(5000)
-        await browser.close();
-        */
